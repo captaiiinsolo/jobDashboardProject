cf2a04052add2dca9032f2f42f2a86a7680d3b9a
captaiiinsolo <solo.d.santos@gmail.com>
2023-01-15 11:28:36 -0800

    remove bulma modal on weather input search

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -138,7 +138,7 @@ $(document).ready(function() {
 $(function () {
   $('#userCitySearch').parsley().on('field:validated', function() {
     var ok = $('.parsley-error').length === 0;
-    $("#weatherModal").toggleClass('is-active', ok);
+    $(".bs-callout-warning").toggleClass('is-hidden', ok);
   })
   .on('form:submit', function() {
     return false; // Don't submit form for this demo
